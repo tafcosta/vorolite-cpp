1f58dc775472c5060fb7ffbd469cdccfc6c98ba6
Tiago Costa <ntc132@CMDADMINs-MacBook-Pro.local>
2025-01-19 15:30:17 +0000

    Add rays class (forgot to add files...)

---
+++ Rays.cpp
@@ -0,0 +1,40 @@
+/*
+ * Rays.cpp
+ *
+ *  Created on: 19 Jan 2025
+ *      Author: Tiago Costa (Newcastle University)
+ */
+
+#include "common_includes.h"
+#include "Rays.h"
+
+Rays::Rays(int nRays, std::vector<double> sourcePosition) : nRays(nRays), sourcePosition(sourcePosition) {
+	direction = std::vector<std::vector<double>>(nRays, std::vector<double>(3, 0.0));
+	initializeDirections();
+	position = std::vector<std::vector<double>>(nRays, sourcePosition);
+
+	opticalDepth = std::vector<double>(nRays, 0.0);
+	numTraversedCells = std::vector<double>(nRays, 0.0);
+	insideDomain = std::vector<bool>(nRays, true);
+
+}
+
+void Rays::initializeDirections() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+
+    for (int iRay = 0; iRay < nRays; ++iRay) {
+        double phi = dis(gen) * 2.0 * M_PI;
+        double theta = std::acos(1.0 - 2.0 * dis(gen));
+
+        direction[iRay][0] = std::cos(phi) * std::sin(theta);
+        direction[iRay][1] = std::sin(phi) * std::sin(theta);
+        direction[iRay][2] = std::cos(theta);
+    }
+}
+
+Rays::~Rays() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Rays.h
@@ -0,0 +1,31 @@
+/*
+ * Rays.h
+ *
+ *  Created on: 19 Jan 2025
+ *      Author: Tiago Costa (Newcastle University)
+ */
+
+#ifndef RAYS_H_
+#define RAYS_H_
+
+class Rays {
+public:
+	Rays(int nRays, std::vector<double> sourcePosition);
+	virtual ~Rays();
+
+	int nRays;
+	std::vector<double> sourcePosition;
+	std::vector<double> phi, theta;
+
+	std::vector<std::vector<double>> direction;
+	std::vector<std::vector<double>> position;
+
+	std::vector<double> opticalDepth;
+	std::vector<double> numTraversedCells;
+	std::vector<bool> insideDomain;
+
+private:
+    void initializeDirections();
+};
+
+#endif /* RAYS_H_ */
