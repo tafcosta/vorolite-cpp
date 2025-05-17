0c196ecd6d09fa394644aa68e1efabc491d79504
Tiago Costa <tafcosta89@gmail.com>
2025-05-17 17:49:39 +0100

    add photochemistry class

---
+++ Photochemistry.cpp
@@ -0,0 +1,18 @@
+/*
+ * Photochemistry.cpp
+ *
+ *  Created on: 17 May 2025
+ *      Author: ntc132
+ */
+
+#include "Photochemistry.h"
+
+Photochemistry::Photochemistry() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Photochemistry::~Photochemistry() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Photochemistry.h
@@ -0,0 +1,17 @@
+/*
+ * Photochemistry.h
+ *
+ *  Created on: 17 May 2025
+ *      Author: ntc132
+ */
+
+#ifndef PHOTOCHEMISTRY_H_
+#define PHOTOCHEMISTRY_H_
+
+class Photochemistry {
+public:
+	Photochemistry();
+	virtual ~Photochemistry();
+};
+
+#endif /* PHOTOCHEMISTRY_H_ */
